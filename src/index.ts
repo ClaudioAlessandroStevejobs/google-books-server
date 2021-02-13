761bbe4c61c482717a745566718132d09231b3f1
VincenzoMars <66218037+VincenzoMars@users.noreply.github.com>
2021-02-13 16:23:34 +0100

    writer router declaration

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import express from 'express';
-
+import express from "express";
+import writerRouter from "./routes/writerRouter";
 const app = express();
 
 const PORT = 3001;
 
 app.listen(PORT, () => {
-    console.log(`Server start on ${PORT}`);
-})
+  console.log(`Server start on ${PORT}`);
+});
