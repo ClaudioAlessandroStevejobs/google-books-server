ab651c1915a87b16a7076ab758a48cf32b3d89b4
VincenzoMars <66218037+VincenzoMars@users.noreply.github.com>
2021-02-13 16:32:00 +0100

    test 2 push/pull

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express from 'express';
-import authRouter from './routes/authRouter'
+import express from "express";
+import authRouter from "./routes/authRouter";
 import writerRouter from "./routes/writerRouter";
 const app = express();
 
 const PORT = 3001;
 
-
+console.log("vincenzo marsssssiano");
 
 app.listen(PORT, () => {
   console.log(`Server start on ${PORT}`);
