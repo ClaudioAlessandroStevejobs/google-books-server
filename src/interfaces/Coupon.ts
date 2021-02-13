d3ac35fc0bc2e618dd187290cc0e61d93dabc75b
Studente <studente@MBP-di-Admin.fritz.box>
2021-02-13 16:57:53 +0100

    adding Coupon Order e Review

---
+++ src/interfaces/Coupon.ts
@@ -0,0 +1,5 @@
+export interface Coupon{
+    id:string;
+    deadline:string;
+    money:number;
+}
---
+++ src/interfaces/Order.ts
@@ -0,0 +1,6 @@
+export interface Order{
+    id:string;
+    date:string;
+    inventory:string[];
+    total:number;
+}
