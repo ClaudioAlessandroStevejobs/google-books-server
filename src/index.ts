9ee3b18600f1aa3f9d260bf8d74ae1ae843b0e8c
ClaudioAlessandroStevejobs <claudio.alessandro@stevejobs.academy>
2021-02-13 16:25:27 +0100

    authRouter delaration

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-
+import authRouter from './routes/authRouter'
 const app = express();
 
 const PORT = 3001;
+
+
 
 app.listen(PORT, () => {
     console.log(`Server start on ${PORT}`);
