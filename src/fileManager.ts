f3676846d994151dd1ee00f64922daee351d3fc3
VincenzoMars <66218037+VincenzoMars@users.noreply.github.com>
2021-02-15 13:26:11 +0100

    isEmailExists() and isPasswordCorrect() added in fileManager (using literals)

--- src/fileManager.ts
+++ src/fileManager.ts
@@ -14,59 +14,105 @@ const readersURI = `${process.cwd()}/files/readers.json`;
  */
 
 export const readBooks = (): Book[] => {
-	const anyArray = JSON.parse(fs.readFileSync(booksURI).toString());
-	let bookArray: Book[] = [];
-	anyArray.map((book: any) => {
-		bookArray.push(
-			new Book(
-				book.title,
-				book.price,
-				book.launchDate,
-				book.genre,
-				book.description,
-				book.authors,
-				book.editors,
-				book.reviews
-			)
-		);
-	});
-	return bookArray;
+  const anyArray = JSON.parse(fs.readFileSync(booksURI).toString());
+  let bookArray: Book[] = [];
+  anyArray.map((book: any) => {
+    bookArray.push(
+      new Book(
+        book.title,
+        book.price,
+        book.launchDate,
+        book.genre,
+        book.description,
+        book.authors,
+        book.editors,
+        book.reviews
+      )
+    );
+  });
+  return bookArray;
 };
 
 export const readWriters = (): Writer[] => {
-	const anyArray = JSON.parse(fs.readFileSync(writersURI).toString());
-	let writerArray: Writer[] = [];
-	anyArray.map(({ email, password, nationality, booksIds, fund, id, token }: User) => {
-		writerArray.push(new Writer(email, password, nationality, fund, booksIds, id, token));
-	})
-	return writerArray;
-}
-
+  const anyArray = JSON.parse(fs.readFileSync(writersURI).toString());
+  let writerArray: Writer[] = [];
+  anyArray.map(
+    ({ email, password, nationality, booksIds, fund, id, token }: User) => {
+      writerArray.push(
+        new Writer(email, password, nationality, fund, booksIds, id, token)
+      );
+    }
+  );
+  return writerArray;
+};
 
 export const readReaders = (): Reader[] => {
-	const anyArray = JSON.parse(fs.readFileSync(readersURI).toString());
-	let readerArray: Reader[] = [];
-	anyArray.map(({ email, id, password, fund, token, booksIds, nationality, orders = [], coupons = [] }: User) => {
-		readerArray.push(new Reader(email, password, nationality, fund, booksIds, orders, coupons, id, token));
-	})
-	return readerArray;
-}
+  const anyArray = JSON.parse(fs.readFileSync(readersURI).toString());
+  let readerArray: Reader[] = [];
+  anyArray.map(
+    ({
+      email,
+      id,
+      password,
+      fund,
+      token,
+      booksIds,
+      nationality,
+      orders = [],
+      coupons = [],
+    }: User) => {
+      readerArray.push(
+        new Reader(
+          email,
+          password,
+          nationality,
+          fund,
+          booksIds,
+          orders,
+          coupons,
+          id,
+          token
+        )
+      );
+    }
+  );
+  return readerArray;
+};
 
 export const getBookById = (iId: string): Book | undefined =>
-	readBooks().find((book: Book) => book.getId() === iId);
+  readBooks().find((book: Book) => book.getId() === iId);
 
 export const getWriterById = (iId: string): Writer | undefined =>
-	readWriters().find((writer: Writer) => writer.getId() === iId);
+  readWriters().find((writer: Writer) => writer.getId() === iId);
 
 export const getReaderById = (iId: string): Reader | undefined =>
-	readReaders().find((reader: Reader) => reader.getId() === iId);
+  readReaders().find((reader: Reader) => reader.getId() === iId);
 
 export const getReaderByToken = (token: string): Reader | undefined =>
-	readReaders().find((reader: Reader) => reader.getToken() === token);
+  readReaders().find((reader: Reader) => reader.getToken() === token);
 
 export const getWriterByToken = (token: string): Writer | undefined =>
-	readWriters().find((writer: Writer) => writer.getToken() === token);
+  readWriters().find((writer: Writer) => writer.getToken() === token);
 
 export const makeOrder = (uId: string, order: Order) => {
-	const readers = readReaders();
+  const readers = readReaders();
 };
+
+export const isEmailExists = (email: string, role: "READER" | "WRITER") =>
+  ({
+    WRITER: readWriters().some((writer: Writer) => writer.getEmail() === email),
+    READER: readReaders().some((reader: Reader) => reader.getEmail() === email),
+  }[role]);
+
+export const isPasswordCorrect = (
+  password: string,
+  role: "READER" | "WRITER"
+) =>
+  ({
+    WRITER: readWriters().some(
+      (writer: Writer) => writer.getPassword() === password
+    ),
+    READER: readReaders().some(
+      (reader: Reader) => reader.getPassword() === password
+    ),
+  }[role]);
